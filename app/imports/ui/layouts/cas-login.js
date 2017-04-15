297cc4d70f8f7b6e782e49ca32c03304e5ac415e
Dan <danli@hawaii.edu>
2017-04-14 16:13:15 -1000

    Added working CAS.

---
+++ app/imports/ui/layouts/cas-login.js
@@ -0,0 +1,41 @@
+/**
+ * Created by danli on 4/14/2017.
+ */
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+Template.Cas_Login.events({
+  /**
+   * Handle the click on the logout link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-logout': function casLogout(event) {
+    event.preventDefault();
+    Meteor.logout();
+    return false;
+  },
+
+  /**
+   * Handle the click on the login link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .cas-login': function casLogin(event, instance) {
+    event.preventDefault();
+    const callback = function loginCallback(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+    Meteor.loginWithCas(callback);
+    return false;
+  },
+});
+
+// Here's how to do the required initialization for Semantic UI dropdown menus.
+Template.Cas_Login.onRendered(function enableDropDown() {
+  this.$('.dropdown').dropdown({
+    action: 'select',
+  });
+});
--- app/imports/ui/layouts/header.js
+++ app/imports/ui/layouts/header.js
@@ -1,7 +1,17 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 // The Header menu does not use dropdown menus, but most menus do.
 // Here's how to do the required initialization for Semantic UI dropdown menus.
 Template.Header.onRendered(function enableDropDown() {
   this.$('.dropdown').dropdown();
+});
+
+Template.Header.helpers({
+  /**
+   * @returns {String} Returns the user who's logged in
+   */
+  user: function user() {
+    return Meteor.user() ? Meteor.user().profile.name : 'No logged in user';
+  },
 });
