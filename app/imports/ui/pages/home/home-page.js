10f3710c6f0c55e9439a0a546435dc0d920682d3
kellyl4 <kellylee@dhcp-168-105-104-112.wireless.manoa.hawaii.edu>
2017-05-09 20:59:30 -1000

    Update

--- app/imports/ui/pages/home/home-page.js
+++ app/imports/ui/pages/home/home-page.js
@@ -5,9 +5,14 @@
 import { Template } from 'meteor/templating';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { _ } from 'meteor/underscore';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Events } from '/imports/api/event/EventCollection';
+
 
 Template.Home_Page.onCreated(function onCreated() {
   this.subscribe(Profiles.getPublicationName());
+  this.subscribe(Events.getPublicationName());
+  this.context = Profiles.getSchema().namedContext('Home_Page');
 });
 
 Template.Home_Page.helpers({
@@ -17,5 +22,11 @@ Template.Home_Page.helpers({
    */
   profiles() {
     return _.first(Profiles.findAll(), 4);
+  },
+  myProfile() {
+    return Profiles.findDoc(FlowRouter.getParam('username'));
+  },
+  events() {
+    return Events.find({}, { sort: { eventName: 1 } });
   },
 });
