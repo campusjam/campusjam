6e3540922bf6f94698f20be9ecc6ebdbf1055ff2
Dan <danli@hawaii.edu>
2017-05-09 18:45:38 -1000

    Update home-page.js.

--- app/imports/ui/pages/home/home-page.js
+++ app/imports/ui/pages/home/home-page.js
@@ -4,6 +4,7 @@
  */
 import { Template } from 'meteor/templating';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { _ } from 'meteor/underscore';
 
 Template.Home_Page.onCreated(function onCreated() {
   this.subscribe(Profiles.getPublicationName());
@@ -15,6 +16,6 @@ Template.Home_Page.helpers({
    * Returns a cursor to profiles, sorted by last name.
    */
   profiles() {
-    return Profiles.find({}, { sort: { firstName: 1 } });
+    return _.first(Profiles.findAll(), 4);
   },
 });
